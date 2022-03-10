7d9b9293690ff84eb14cd71a2a57a3e4d02a3749
robayris <robayris@gmail.com>
2022-03-11 09:50:28 +1100

    Add MDB

--- js/scripts.js
+++ js/scripts.js
@@ -52,3 +52,26 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+const pickr2 = new Pickr({
+    el: '#color-picker-2',
+    useAsButton: true,
+    default: "00FF00",
+
+    components: {
+    preview: true,
+    opacity: true,
+    hue: true,
+
+    interaction: {
+        hex: true,
+        rgba: true,
+        hsla: true,
+        hsva: true,
+        cmyk: true,
+        input: true,
+        clear: true,
+        save: false
+    }
+    }
+});
