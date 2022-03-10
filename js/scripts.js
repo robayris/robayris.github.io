edddbedd8657c5f740b72abce39c545ef6618223
robayris <robayris@gmail.com>
2022-03-11 09:59:33 +1100

    remove MDB

--- js/scripts.js
+++ js/scripts.js
@@ -52,26 +52,3 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
-
-const pickr2 = new Pickr({
-    el: '#color-picker-2',
-    useAsButton: true,
-    default: "00FF00",
-
-    components: {
-    preview: true,
-    opacity: true,
-    hue: true,
-
-    interaction: {
-        hex: true,
-        rgba: true,
-        hsla: true,
-        hsva: true,
-        cmyk: true,
-        input: true,
-        clear: true,
-        save: false
-    }
-    }
-});
